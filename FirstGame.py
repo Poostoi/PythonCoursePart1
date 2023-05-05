e75c2341b6e2142406f98c8b87cfc6ef6e35282e
Poostoi <oleg.nour@bk.ru>
2023-05-05 14:21:17 +0300

    edit first game and add new file game

--- FirstGame.py
+++ FirstGame.py
@@ -23,7 +23,7 @@ display.fill(array_colors[0])
 # Основной цикл игры
 FPS = 60  # Создаем переменную FPS
 clock = pg.time.Clock()  # Создаем счетчик для FPS
-
+size_cursor = 20
 current_color = 1
 while True:
 
@@ -34,21 +34,28 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                pg.draw.circle(
-                    display, array_colors[current_color], event.pos, 20)
-                pg.display.update()
-            elif event.button == 3:
-                if (current_color == 8):
-                    current_color = 1
-                else:
-                    current_color += 1
-                pg.display.update()
-            elif event.button == 2:
-                pg.draw.circle(
-                    display, array_colors[0], event.pos, 20)
-                pg.display.update()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                size_cursor += 3
+            elif event.key == pg.K_e:
+                size_cursor -= 3
+
+    keys_mouse = pg.mouse.get_pressed();
+    keys_keyword = pg.key.get_pressed()
+    if keys_mouse[0]:
+        pg.draw.circle(
+            display, array_colors[current_color], pg.mouse.get_pos(), size_cursor)
+        pg.display.update()
+    elif keys_mouse[1]:
+        pg.draw.circle(
+            display, array_colors[0], event.pos, size_cursor)
+        pg.display.update()
+    elif keys_mouse[2]:
+        if (current_color == 8):
+            current_color = 1
+        else:
+            current_color += 1
+
 
     # Обновляем поверхность игры
     # на каждом шаге основного цикла игры
